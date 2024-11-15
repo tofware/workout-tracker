d7cb7fdd77ed8414c159524b044cbee20db23ae8
Tofan <tofanalin1@gmail.com>
2024-11-15 11:19:29 +0200

    Installed ide helper

--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -3,10 +3,36 @@
 namespace App\Models;
 
 use Database\Factories\WorkoutFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
+/**
+ *
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $category_id
+ * @property string $name
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Category $category
+ * @property-read User $user
+ * @method static WorkoutFactory factory($count = null, $state = [])
+ * @method static Builder<static>|Workout newModelQuery()
+ * @method static Builder<static>|Workout newQuery()
+ * @method static Builder<static>|Workout query()
+ * @method static Builder<static>|Workout whereCategoryId($value)
+ * @method static Builder<static>|Workout whereCreatedAt($value)
+ * @method static Builder<static>|Workout whereId($value)
+ * @method static Builder<static>|Workout whereName($value)
+ * @method static Builder<static>|Workout whereUpdatedAt($value)
+ * @method static Builder<static>|Workout whereUserId($value)
+ * @mixin Eloquent
+ */
 class Workout extends Model
 {
     /** @use HasFactory<WorkoutFactory> */
